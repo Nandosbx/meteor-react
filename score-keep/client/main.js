b21d25e1c608dbf2a29f2d8815279c7dcbcbb4c7
Fernando Batista <nandosbx@gmail.com>
2020-12-07 00:51:42 -0300

    Meteor React React-DOM

--- score-keep/client/main.js
+++ score-keep/client/main.js
@@ -1,1 +1,8 @@
-console.log('Log from /client/main.js')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Meteor } from 'meteor/meteor'
+
+Meteor.startup(function () {
+    let jsx = <p>This is from main.js</p>
+    ReactDOM.render(jsx, document.getElementById('app'))
+})
