1103ff7836cf45f961ecdb05caa9cdeb0834d259
Fernando Batista <nandosbx@gmail.com>
2020-12-09 00:02:12 -0300

    Mini Mongo Collection

--- score-keep/server/main.js
+++ score-keep/server/main.js
@@ -1,1 +1,10 @@
-import {Players} frin './../imports/api/players'
+import { Meteor } from 'meteor/meteor'
+import { Players } from './../imports/api/players'
+
+Meteor.startup(function () {
+    Players.insert({
+        name: 'John',
+        score: 3,
+    })
+    console.log(Players.find().fetch())
+})
