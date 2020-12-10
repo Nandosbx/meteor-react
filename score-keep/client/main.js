df534db0b3429918526528d3dede3a6f2a7d68e1
Fernando Batista <nandosbx@gmail.com>
2020-12-09 23:42:35 -0300

    Quering Data

--- score-keep/client/main.js
+++ score-keep/client/main.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Meteor } from 'meteor/meteor'
+import { Tracker } from 'meteor/tracker'
+
+import { Players } from './../imports/api/players'
+
+Tracker.autorun(function () {
+    console.log('Player List:', Players.find().fetch())
+})
+
+/*setTimeout(function () {
+    console.log('Player List:', Players.find().fetch())
+})*/
 
 const players = [
     {
@@ -17,6 +28,12 @@ const players = [
         _id: '3',
         name: 'Andrew',
         score: -12,
+    },
+
+    {
+        _id: '4',
+        name: 'John',
+        score: 12,
     },
 ]
 
