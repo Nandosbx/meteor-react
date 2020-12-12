d6a3a23caa2887a6d0933a49fac8bd1600ed602d
Fernando Batista <nandosbx@gmail.com>
2020-12-11 22:28:08 -0300

    Rendering Players

--- score-keep/client/main.js
+++ score-keep/client/main.js
@@ -5,49 +5,21 @@ import { Tracker } from 'meteor/tracker'
 
 import { Players } from './../imports/api/players'
 
-Tracker.autorun(function () {
-    console.log('Player List:', Players.find().fetch())
-})
-
-/*setTimeout(function () {
-    console.log('Player List:', Players.find().fetch())
-})*/
-
-const players = [
-    {
-        _id: '1',
-        name: 'Lauren',
-        score: 99,
-    },
-    {
-        _id: '2',
-        name: 'Cory',
-        score: -1,
-    },
-    {
-        _id: '3',
-        name: 'Andrew',
-        score: -12,
-    },
-
-    {
-        _id: '4',
-        name: 'John',
-        score: 12,
-    },
-]
-
 const renderPlayers = function (playersList) {
     return playersList.map(function (player) {
         return (
             <p key={player._id}>
-                {player.name} has {player.score}{' '}
+                {player.name} has {player.score} point(s){' '}
             </p>
         )
     })
 }
 
 Meteor.startup(function () {
-    let jsx = <div>{renderPlayers(players)}</div>
+    Tracker.autorun(() => {
+        let players = Players.find().fetch()
+        let jsx = <div>{renderPlayers(players)}</div>
+    })
+
     ReactDOM.render(jsx, document.getElementById('app'))
 })
